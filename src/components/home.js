296a3ee44b4aa27b7e4096cfbab5c738927ad03b
Lewis <hamiltonjlewis@gmail.com>
2021-08-20 08:57:47 -0700

    homepage test

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Please Enter Dollar amount to add to savings</h1>
+      <input></input>
+      <button>Calculate</button>
+    </div>
+  );
+}
