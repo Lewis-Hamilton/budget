ed4cc4d2b25e88b4259611b772f316e466ddb307
Lewis <hamiltonjlewis@gmail.com>
2021-08-21 13:45:25 -0700

    added to spending component

--- src/components/spending.js
+++ src/components/spending.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 export default function Spending() {
+  const [totalSpending, setTotalSpending] = useState(0);
+  const [dailySpending, setDailypending] = useState(0);
+  const [bigSpending, setBigSpending] = useState(0);
   return (
-    <div>
-      <h1>SPENDING</h1>
-    </div>
+    <Grid>
+      <Grid item>
+        <Typography>Total Spending Amount</Typography>
+        <Typography>{totalSpending}</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>Daily Spending Amount</Typography>
+        <Typography>{dailySpending}</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>Big Spending Amount</Typography>
+        <Typography>{bigSpending}</Typography>
+      </Grid>
+    </Grid>
   );
 }
