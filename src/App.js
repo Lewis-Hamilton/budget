e1553c0b913bc18bcea87336f611cd8290ce626b
Lewis <hamiltonjlewis@gmail.com>
2021-09-02 08:42:31 -0700

    moved style to app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import Saving from "./components/saving.js";
 import Spending from "./components/spending.js";
 import Goals from "./components/goals.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "linear-gradient(315deg, #20bf55 0%, #01baef 74%)",
+    height: "100vh",
+    color: "white",
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
   return (
     <Router>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/saving" component={Saving}></Route>
-        <Route exact path="/spending" component={Spending}></Route>
-        <Route exact path="/goals" component={Goals}></Route>
-      </Switch>
+      <Box className={classes.root}>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/saving" component={Saving}></Route>
+          <Route exact path="/spending" component={Spending}></Route>
+          <Route exact path="/goals" component={Goals}></Route>
+        </Switch>
+      </Box>
     </Router>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTest } from "../reducers/rootReducer";
-import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    background: "linear-gradient(45deg, #00a900 30%, #006453 90%)",
-    height: "100vh",
-  },
-}));
-
 export default function Home() {
   const [initialAmount, setInitialAmount] = useState(0);
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
+    <>
       <Typography>Please Enter Dollar amount to add to savings</Typography>
       <TextField
         variant="outlined"
@@ -29,6 +19,6 @@ export default function Home() {
       <Button onClick={() => dispatch(addTest(initialAmount))}>
         Calculate
       </Button>
-    </Box>
+    </>
   );
 }
