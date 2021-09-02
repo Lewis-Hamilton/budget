f37c7c7651c9d2142e8b281c65aa9a36fdfabaac
Lewis <hamiltonjlewis@gmail.com>
2021-09-02 09:33:33 -0700

    fixed style for home

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTest } from "../reducers/rootReducer";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  grid: {
+    position: "absolute",
+    top: "50%",
+  },
+}));
 
 export default function Home() {
   const [initialAmount, setInitialAmount] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   return (
-    <>
-      <Typography>Please Enter Dollar amount to add to savings</Typography>
-      <TextField
-        variant="outlined"
-        onChange={(e) => setInitialAmount(e.target.value)}
-      ></TextField>
-      <Button onClick={() => dispatch(addTest(initialAmount))}>
-        Calculate
-      </Button>
-    </>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      spacing={2}
+      className={classes.grid}
+    >
+      <Grid item>
+        <TextField
+          variant="outlined"
+          label="Please Enter Paycheck Amount"
+          onChange={(e) => setInitialAmount(e.target.value)}
+        ></TextField>
+      </Grid>
+      <Grid item>
+        <Button
+          size="large"
+          variant="contained"
+          onClick={() => dispatch(addTest(initialAmount))}
+        >
+          Calculate
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
